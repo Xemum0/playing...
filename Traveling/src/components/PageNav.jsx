7a156219d4844b744171c0e21789e6d9a010abe9
youcef mhammdi bouzina <youcef.mhammdi.bouzina@ensia.edu.dz>
2024-12-26 17:02:54 +0100

    css modules

--- Traveling/src/components/PageNav.jsx
+++ Traveling/src/components/PageNav.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-
+import styles from "./PageNav.module.css"
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link to={"/"}>Home</Link>
