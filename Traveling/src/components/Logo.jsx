96ce1651792731cf4d0f42e7fc6e74039cd36eea
youcef mhammdi bouzina <youcef.mhammdi.bouzina@ensia.edu.dz>
2024-12-26 22:18:03 +0100

    commit

--- Traveling/src/components/Logo.jsx
+++ Traveling/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import styles from "./Logo.module.css";
 
 function Logo() {
-  return <img src="/" alt="Traveling logo" className={styles.logo} />;
+  return <img src="/logo.png" alt="Traveling logo" className={styles.logo} />;
 }
 
 export default Logo;
